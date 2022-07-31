d8bab80ea2b4c4d01747fb4656ddfa5927d77410
S1M0N38 <bertolottosimone@gmail.com>
2022-07-31 17:05:23 +0200

    Use gruvbox as colorscheme
    I like gruvbox. `ellisonleao/gruvbox.nvim` supports treesitter syntax
    highlighting and it's actively maintained. I use gruvbox theme
    throughout my dots files ('exa', 'fzf', 'bat', 'alacritty'): it's nice
    to have it also in Neovim.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,12 @@
-local colorscheme = "darkplus"
+-- Using gruvbox colorscheme :
+-- -- dark and light version supported 
+-- -- syntax highlighting using treesitter
+
+local colorscheme = "gruvbox"
+vim.o.background = "dark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+  vim.notify("Couldn't set " .. colorscheme, "error")
   return
 end
