b86b92e289bc8e9c9548a6437dc2d01428d15d51
S1M0N38 <bertolottosimone@gmail.com>
2022-07-31 17:31:03 +0200

    Nvim-tree for hierarchical project navigation
    Sometimes you need to explore a project (collection of files) organized
    in nested directories. If you don't know how bits are linked together,
    tree navigation is the way to go. This plugin improve over the build in
    explorer exposing a tree-like view enriched with beautiful icons, git
    annotations and diagnostic annotations.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,36 +1,14 @@
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
+local status_nvim_tree, nvim_tree = pcall(require, "nvim-tree")
+if not status_nvim_tree then
+  vim.notify("Couldn't load nvim-tree" .. nvim_tree, "error")
   return
 end
-
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
+
   renderer = {
-    root_folder_modifier = ":t",
     icons = {
       glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
         git = {
           unstaged = "",
           staged = "S",
@@ -43,6 +21,7 @@ nvim_tree.setup {
       },
     },
   },
+
   diagnostics = {
     enable = true,
     show_on_dirs = true,
@@ -53,16 +32,9 @@ nvim_tree.setup {
       error = "",
     },
   },
-  view = {
-    width = 30,
-    height = 30,
-    side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
+
+  git = {
+    enable = true,
   },
+
 }
