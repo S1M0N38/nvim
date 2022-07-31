11b83834157e44e71a697bf8ab96549f494d822a
S1M0N38 <bertolottosimone@gmail.com>
2022-07-31 14:52:45 +0200

    Impatient.nvim improve Neovim startup time
    This plugin perform some sorceries in the background and reduce the
    Neovim startup time. On my machine with this configuration it translate
    to ~50ms reduction (~25% improvement).

--- lua/user/impatient.lua
+++ lua/user/impatient.lua
@@ -1,6 +1,18 @@
-local status_ok, impatient = pcall(require, "impatient")
-if not status_ok then
-  return
+_G.__luacache_config = {
+  chunks = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_chunks',
+  },
+  modpaths = {
+    enable = true,
+    path = vim.fn.stdpath('cache')..'/luacache_modpaths',
+  }
+}
+
+local status_impatient, impatient = pcall(require, "impatient")
+if not status_impatient then
+  vim.notify("Couldn't load impatient" .. impatient, "error")
+	return
 end
 
-impatient.enable_profile()
+-- impatient.enable_profile()
