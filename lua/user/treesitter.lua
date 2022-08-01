2a718488395cad320527f7f8acf8e15446f42ef4
S1M0N38 <bertolottosimone@gmail.com>
2022-08-01 11:38:47 +0200

    Tree-sitter as parsing library
    It's a tool that parse the code you're writing (it understand what is a
    function, what is a variable, the scope you are working on, etc...)
    This understanding can be used for:
    - improve syntax highlighting
    - code navigation
    - refactoring
    and more.
    Installation is as simple as `:TSInstall <language>`.
    For a list of available languages `:TSInstallInfo`.

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,17 +1,41 @@
+require "user.keymaps" -- import TS_KEYMAPS
+
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  vim.notify("Couldn't load nvim-treesitter.configs" .. configs, "error")
+  return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
+
+  ensure_installed = { "lua", "python" },
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+
+  incremental_selection = {
+    enable = true,
+    -- keymaps = TS_KEYMAPS,
+  },
+
+  indent = {
+    enable = true
+  },
+
+  -- autopairs = {
+  --   enable = true,
+  -- },
+
+})
+
+-- Bug for packer.nvim users https://github.com/nvim-treesitter/nvim-treesitter/issues/1469
+-- workaround: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+  end
 })
